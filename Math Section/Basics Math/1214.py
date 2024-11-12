337b13c31e253b985ddd1de88b2d75f5d7d54160
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2024-11-13 02:03:57 +0600

    Large Division( Easy )

---
+++ Math Section/Basics Math/1214.py
@@ -0,0 +1,13 @@
+def basic_io(s, n):
+    if s % n == 0:
+        print("divisible")
+    else:
+        print("not divisible")
+
+def main():
+    t = int(input())
+    for i in range(1, t + 1):
+        print("Case " + str(i) + ":", end=" ")
+        s, n = map(int, input().split())
+        basic_io(s, n)
+main()
